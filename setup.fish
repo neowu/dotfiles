4b567347a6acc3df9a9f71bfd97e6f79aa1e854a
neo <1100909+neowu@users.noreply.github.com>
2025-03-04 10:42:44 +0800

    update fish theme

--- setup.fish
+++ setup.fish
@@ -16,7 +16,7 @@ set -U fish_color_comment --dim black
 set -U fish_color_error red
 set -U fish_color_gray --dim black
 set -U fish_color_selection --background=brwhite
-set -U fish_color_search_match --background=brwhite
+set -U fish_color_search_match --reverse
 set -U fish_color_option cyan
 set -U fish_color_operator magenta
 set -U fish_color_escape brred
@@ -26,10 +26,20 @@ set -U fish_color_cwd cyan
 set -U fish_color_user cyan
 set -U fish_color_host blue
 set -U fish_color_status red
-set -U fish_pager_color_progress --dim black
-set -U fish_pager_color_prefix blue
+
+set -U fish_pager_color_background
 set -U fish_pager_color_completion normal
 set -U fish_pager_color_description yellow
+set -U fish_pager_color_prefix blue
+set -U fish_pager_color_progress --dim black
+set -U fish_pager_color_secondary_background
+set -U fish_pager_color_secondary_completion
+set -U fish_pager_color_secondary_description
+set -U fish_pager_color_secondary_prefix
+set -U fish_pager_color_selected_background --reverse
+set -U fish_pager_color_selected_completion
+set -U fish_pager_color_selected_description
+set -U fish_pager_color_selected_prefix
 
 switch (hostname)
 case '*dev*'
