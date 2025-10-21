b51d46b2f2e07eab3e2e18b5f305ac0a1f8289c6
neo <1100909+neowu@users.noreply.github.com>
2025-10-21 15:25:54 +0800

    switch to Nord theme

--- archive/zed/theme/generate.js
+++ archive/zed/theme/generate.js
@@ -1,7 +1,7 @@
 // node zed/theme/generate.js
 import fs from "node:fs/promises";
 
-const DARK0 = "#2e3440"; // it is used for background and area coloring while it's not used for syntax highlighting at all because otherwise it would collide with the same background color.
+const DARK0 = "#2e3440"; // Used for texts, backgrounds, carets and structuring characters like curly- and square brackets.
 const DARK1 = "#3b4252"; // Used as a lighter background color for UI elements like status bars.
 const DARK2 = "#434c5e"; // Used as line highlighting in the editor. In the UI scope it may be used as selection- and highlight color.
 const DARK3 = "#4c566a"; // Used for comments, invisibles, indent- and wrap guide marker. In the UI scope used as pseudoclass color for disabled elements.
@@ -27,78 +27,193 @@ const PURPLE0 = "#b48ead"; // Used for numbers.
 
 const TRANSPARENT = "#00000000";
 
+function shade(color, percent) {
+  var R = parseInt(color.substring(1, 3), 16);
+  var G = parseInt(color.substring(3, 5), 16);
+  var B = parseInt(color.substring(5, 7), 16);
+
+  R = parseInt((R * (100 + percent)) / 100);
+  G = parseInt((G * (100 + percent)) / 100);
+  B = parseInt((B * (100 + percent)) / 100);
+
+  R = R < 255 ? R : 255;
+  G = G < 255 ? G : 255;
+  B = B < 255 ? B : 255;
+
+  R = Math.round(R);
+  G = Math.round(G);
+  B = Math.round(B);
+
+  var RR = R.toString(16).length == 1 ? "0" + R.toString(16) : R.toString(16);
+  var GG = G.toString(16).length == 1 ? "0" + G.toString(16) : G.toString(16);
+  var BB = B.toString(16).length == 1 ? "0" + B.toString(16) : B.toString(16);
+
+  return "#" + RR + GG + BB;
+}
+
+const BG = "#090E13";
+const BG1 = "#1C1E25";
+const BG2 = "#22262D";
+const BG3 = "#393B44";
+// const BG4 = "#4B4D55";
+
+// Popup and Floats
+const ALT_BLUE1 = "#223249";
+const ALT_BLUE2 = "#2D4F67";
+
+// Main Colors
+const RED = "#C34043";
+const RED2 = "#E46876";
+const RED3 = "#c4746e";
+const YELLOW = "#DCA561";
+const YELLOW2 = "#E6C384";
+const YELLOW3 = "#c4b28a";
+const YELLOW4 = "#887244";
+const GREEN = "#98BB6C";
+const GREEN2 = "#87a987";
+const GREEN3 = "#8a9a7b";
+const GREEN4 = "#6A9589";
+const GREEN5 = "#7AA89F";
+const BLUE = "#7FB4CA";
+const BLUE2 = "#658594";
+const BLUE3 = "#8ba4b0";
+
+const VIOLET = "#938AA9";
+const VIOLET2 = "#8992a7";
+const VIOLET3 = "#949fb5";
+const PINK = "#a292a3";
+const PINK1 = "#C6BDC7";
+const ORANGE = "#b6927b";
+const ORANGE2 = "#b98d7b";
+const CYAN = "#8ea4a2";
+
+// Fg and Comments
+const FG2 = "#f2f1ef";
+const FG = "#C5C9C7";
+const GRAY = "#717C7C";
+const GRAY2 = "#A4A7A4"; // silver
+const GRAY3 = "#909398";
+const GRAY4 = "#75797f";
+const GRAY5 = "#5C6066";
+
+// Light theme colors (pearl prefix)
+const L_FG = BG2;
+const PEARL_BLACK1 = "#545464";
+const PEARL_BLACK2 = "#43436c";
+const PEARL_GRAY = "#e2e1df";
+const PEARL_GRAY2 = "#5C6068";
+const L_GRAY3 = "#6D6D69";
+const L_GRAY4 = "#9F9F99";
+
+const L_BG = "#f2f1ef";
+const L_BG1 = "#e2e1df";
+const L_BG2 = "#dddddb";
+const L_BG3 = "#EBEBEA";
+
+const PEARL_WHITE3 = "#cacac7";
+const L_VIOLET1 = "#a09cac";
+const L_VIOLET2 = "#766b90";
+const L_VIOLET3 = "#624c83";
+
+const L_BLUE1 = "#c7d7e0";
+const L_BLUE2 = "#b5cbd2";
+const L_BLUE3 = "#9fb5c9";
+const L_BLUE4 = "#4d699b";
+const PEARL_BLUE5 = "#5d57a3";
+const L_GREEN = "#6f894e";
+const L_GREEN2 = "#6e915f";
+const PEARL_GREEN3 = "#b7d0ae";
+const L_PINK = "#b35b79";
+const L_ORANGE = "#cc6d00";
+const PEARL_ORANGE2 = "#e98a00";
+const L_YELLOW = "#77713f";
+const L_YELLOW2 = "#836f4a";
+const L_YELLOW3 = "#de9800";
+const PEARL_YELLOW4 = "#f9d791";
+const L_RED = "#c84053";
+const L_RED2 = "#d7474b";
+const L_RED3 = "#e82424";
+const L_RED4 = "#d9a594";
+const L_AQUA = "#597B75";
+const L_AQUA2 = "#5e857a";
+const L_TEAL1 = "#4e8ca2";
+const L_TEAL2 = "#6693bf";
+const PEARL_TEAL3 = "#5a7785";
+const PEARL_CYAN = "#d7e3d8";
+
 let theme_data = themes();
-await fs.writeFile("./nord_theme.json", JSON.stringify(theme_data, null, 2));
-await fs.writeFile("/Users/neo/.config/zed/themes/nord_theme.json", JSON.stringify(theme_data, null, 2));
+await fs.writeFile("./neo_theme.json", JSON.stringify(theme_data, null, 2));
+await fs.writeFile("/Users/neo/.config/zed/themes/neo_theme.json", JSON.stringify(theme_data, null, 2));
 
 function themes() {
   return {
     $schema: "https://zed.dev/schema/themes/v0.2.0.json",
-    name: "Nord",
+    name: "Neo",
     author: "Neo",
-    themes: [theme()],
+    themes: [theme(true), theme(false)],
   };
 }
 
-function theme() {
-  let foreground = LIGHT2;
-  let background = shade(DARK0, -30);
-  let border = DARK1;
-  let active = DARK1;
-  let highlight = DARK2;
-  let click = DARK3;
-  let select = shade(DARK3, 50) + "80";
-  let scrollbar = shade(DARK3, 50);
+function theme(dark) {
+  let foreground = dark ? LIGHT0 : L_FG;
+  let background = dark ? shade(DARK0, -30) : L_BG;
+  let border = dark ? DARK1 : L_BG1;
+  let active = dark ? DARK1 : L_BG2;
+  let highlight = dark ? DARK2 : L_VIOLET1 + "33";
+  let click = dark ? DARK3 : L_BG3;
+  let select = dark ? FROST1 + "80" : L_BLUE3 + "80";
 
-  let accent = FROST1;
-  let text_muted = shade(LIGHT0, -20);
+  let accent = dark ? FROST1 : L_AQUA2;
+  let text_muted = dark ? DARK3 : L_GRAY3;
 
-  let red = RED0;
-  let br_red = shade(red, 20);
-  let yellow = YELLOW0;
-  let br_yellow = shade(yellow, 20);
-  let dim_yellow = shade(yellow, -30);
-  let blue = FROST2;
-  let br_blue = shade(blue, 20);
-  let green = GREEN0;
-  let br_green = shade(green, 20);
-  let magenta = PURPLE0;
-  let br_magenta = shade(magenta, 20);
-  let cyan = FROST1;
-  let br_cyan = FROST0;
+  let red = dark ? RED0 : L_RED;
+  let br_red = dark ? RED2 : L_RED2;
 
-  let orange = ORANGE0;
+  let yellow = dark ? YELLOW0 : L_YELLOW;
+  let br_yellow = dark ? YELLOW2 : L_YELLOW3;
+  let dark_yellow = dark ? YELLOW4 : L_YELLOW2;
 
-  let white = LIGHT0;
-  let br_white = LIGHT2;
-  let dim_white = shade(white, -30);
+  let blue = dark ? FROST2 : L_BLUE4;
+  let br_blue = dark ? BLUE : L_BLUE3;
 
-  let gray = shade(DARK3, 20);
-  let comment = shade(LIGHT2, -40);
+  let green = dark ? GREEN3 : L_GREEN;
+  let br_green = dark ? GREEN2 : L_GREEN2;
+
+  let magenta = dark ? PINK : L_VIOLET2;
+  let br_magenta = dark ? PINK1 : L_VIOLET3;
+
+  let cyan = dark ? FROST1 : L_AQUA;
+  let br_cyan = dark ? GREEN5 : L_TEAL1;
+
+  let orange = dark ? ORANGE0 : L_ORANGE;
+  let pink = dark ? PINK : L_PINK;
+  let violet = dark ? VIOLET2 : L_VIOLET3;
+
+  let gray = dark ? DARK3 : L_GRAY3;
 
   return {
-    name: "Nord Dark",
-    appearance: "dark",
+    name: dark ? "Neo Dark" : "Neo Light",
+    appearance: dark ? "dark" : "light",
     style: {
-      accents: [blue, cyan, red, orange, yellow, green, magenta],
+      accents: [blue, orange, magenta, green, pink, yellow, cyan, red],
 
       background: background,
 
       border: border,
-      "border.variant": border + "a0", // Border color. Used for deemphasized borders, like a visual divider between two sections
+      "border.variant": border + "a0",
       "border.focused": border,
       "border.selected": border,
       "border.transparent": TRANSPARENT,
-      "border.disabled": DARK3,
+      "border.disabled": border + "80",
 
       "drop_target.background": border,
 
       "editor.foreground": foreground,
       "editor.background": background,
-      "editor.gutter.background": background, // editor left pane, e.g. line number
+      "editor.gutter.background": background,
       "editor.subheader.background": border,
-      "editor.active_line.background": active + "80",
-      "editor.highlighted_line.background": null,
+      "editor.active_line.background": active + "a0",
+      "editor.highlighted_line.background": active + "a0",
       "editor.line_number": DARK3,
       "editor.active_line_number": foreground,
       "editor.invisible": active, // Text Color. Used to mark invisible characters in the editor. Example: spaces, tabs, carriage returns, etc.
@@ -110,15 +225,15 @@ function theme() {
 
       "element.background": active,
       "element.hover": active + "80",
-      "element.active": active, // e.g. setting UI, left list
+      "element.active": active,
       "element.selected": active,
       "element.disabled": border,
 
       "elevated_surface.background": border, // code completion popup
 
       "ghost_element.background": TRANSPARENT,
-      "ghost_element.selected": click, // code completion popup, keyboard selection background
-      "ghost_element.hover": click, // tab delete hover, code completion popup hover
+      "ghost_element.selected": active,
+      "ghost_element.hover": click,
       "ghost_element.active": click,
       "ghost_element.disabled": border,
 
@@ -141,15 +256,15 @@ function theme() {
       "toolbar.background": background,
 
       "panel.background": background,
-      "panel.focused_border": null,
-      "pane.focused_border": null,
+      "panel.focused_border": dark ? BLUE3 : L_BG2,
+      "pane.focused_border": dark ? VIOLET3 : L_BG2,
 
-      "scrollbar.thumb.background": scrollbar + "60",
-      "scrollbar.thumb.hover_background": scrollbar + "80",
-      "scrollbar.thumb.active_background": scrollbar + "80",
+      "scrollbar.thumb.background": click + "40",
+      "scrollbar.thumb.hover_background": click + "80",
+      "scrollbar.thumb.active_background": click + "80",
       "scrollbar.thumb.border": TRANSPARENT,
       "scrollbar.track.background": background,
-      "scrollbar.track.hover_background": scrollbar + "80",
+      "scrollbar.track.hover_background": click + "80",
       "scrollbar.track.border": TRANSPARENT,
 
       "search.match_background": highlight,
@@ -158,48 +273,55 @@ function theme() {
 
       "terminal.background": background,
       "terminal.foreground": foreground,
-      "terminal.bright_foreground": null,
+      "terminal.bright_foreground": dark ? YELLOW3 : L_FG,
 
-      "terminal.ansi.black": DARK1,
-      "terminal.ansi.bright_black": DARK3,
+      "terminal.ansi.black": dark ? BG1 : L_FG,
+      "terminal.ansi.bright_black": dark ? BG2 : L_FG,
+
       "terminal.ansi.red": red,
       "terminal.ansi.bright_red": br_red,
+
       "terminal.ansi.green": green,
       "terminal.ansi.bright_green": br_green,
+
       "terminal.ansi.yellow": yellow,
       "terminal.ansi.bright_yellow": br_yellow,
+
       "terminal.ansi.blue": blue,
       "terminal.ansi.bright_blue": br_blue,
+
       "terminal.ansi.magenta": magenta,
       "terminal.ansi.bright_magenta": br_magenta,
+
       "terminal.ansi.cyan": cyan,
       "terminal.ansi.bright_cyan": br_cyan,
-      "terminal.ansi.white": white,
-      "terminal.ansi.bright_white": br_white,
 
-      "link_text.hover": br_cyan,
+      "terminal.ansi.white": dark ? FG : L_BG1,
+      "terminal.ansi.bright_white": dark ? ORANGE : L_BG,
+
+      "link_text.hover": GREEN4,
 
       conflict: red,
       created: green,
       deleted: red,
       error: red,
-      "error.border": red,
-      "error.background": red + "33",
       warning: yellow,
-      "warning.border": yellow,
-      "warning.background": yellow + "33",
-      hidden: dim_white,
+      "warning.border": active,
+      "warning.background": border,
+      hidden: violet,
       success: br_green,
       "success.border": active,
       "success.background": border,
-      info: dim_white,
+      info: violet,
       "info.border": active,
       "info.background": border,
-      hint: dim_white, // git blame text
+      hint: violet,
+      "hint.border": active,
+      "hint.background": border,
       modified: yellow,
       renamed: orange,
       unreachable: foreground,
-      ignored: dim_yellow, // git ignored files in file tree
+      ignored: dark_yellow,
       predictive: gray,
 
       players: [
@@ -209,46 +331,49 @@ function theme() {
           selection: select,
         },
         {
-          cursor: cyan,
-        },
-        {
           cursor: blue,
-        },
-        {
-          cursor: red,
         },
         {
           cursor: orange,
         },
         {
-          cursor: yellow,
+          cursor: magenta,
         },
         {
           cursor: green,
         },
         {
-          cursor: magenta,
+          cursor: pink,
+        },
+        {
+          cursor: yellow,
+        },
+        {
+          cursor: cyan,
+        },
+        {
+          cursor: red,
         },
       ],
 
       syntax: {
         attribute: {
-          color: cyan,
+          color: violet,
         },
         boolean: {
-          color: blue,
+          color: orange,
         },
         comment: {
-          color: comment,
+          color: gray,
         },
         "comment.doc": {
-          color: comment,
+          color: gray,
         },
         constant: {
-          color: white,
+          color: orange,
         },
         constructor: {
-          color: blue,
+          color: violet,
         },
         embedded: {
           color: orange,
@@ -267,20 +392,20 @@ function theme() {
           font_weight: 300,
         },
         keyword: {
-          color: blue,
+          color: violet,
         },
         label: {
-          color: blue,
+          color: violet,
         },
         link_text: {
-          color: blue,
+          color: gray,
           font_style: "normal",
         },
         link_uri: {
-          color: blue,
+          color: violet,
         },
         number: {
-          color: magenta,
+          color: pink,
         },
         operator: {
           color: blue,
@@ -295,7 +420,7 @@ function theme() {
           color: foreground,
         },
         property: {
-          color: cyan,
+          color: yellow,
         },
         punctuation: {
           color: blue,
@@ -321,7 +446,7 @@ function theme() {
           color: green,
         },
         "string.escape": {
-          color: yellow,
+          color: blue,
           font_weight: 700,
         },
         "string.regex": {
@@ -331,58 +456,34 @@ function theme() {
           color: green,
         },
         tag: {
-          color: blue,
+          color: yellow,
         },
         "text.literal": {
           color: foreground,
         },
         title: {
-          color: cyan,
+          color: violet,
           font_weight: 700,
         },
         type: {
-          color: br_cyan,
+          color: cyan,
         },
         "type.builtin": {
-          color: br_cyan,
+          color: cyan,
         },
         "type.super": {
-          color: br_cyan,
+          color: cyan,
         },
         variable: {
-          color: white,
+          color: foreground,
         },
         "variable.parameter": {
-          color: white,
+          color: yellow,
         },
         "variable.special": {
-          color: white,
+          color: yellow,
         },
       },
     },
   };
-}
-
-function shade(color, percent) {
-  var R = parseInt(color.substring(1, 3), 16);
-  var G = parseInt(color.substring(3, 5), 16);
-  var B = parseInt(color.substring(5, 7), 16);
-
-  R = parseInt((R * (100 + percent)) / 100);
-  G = parseInt((G * (100 + percent)) / 100);
-  B = parseInt((B * (100 + percent)) / 100);
-
-  R = R < 255 ? R : 255;
-  G = G < 255 ? G : 255;
-  B = B < 255 ? B : 255;
-
-  R = Math.round(R);
-  G = Math.round(G);
-  B = Math.round(B);
-
-  var RR = R.toString(16).length == 1 ? "0" + R.toString(16) : R.toString(16);
-  var GG = G.toString(16).length == 1 ? "0" + G.toString(16) : G.toString(16);
-  var BB = B.toString(16).length == 1 ? "0" + B.toString(16) : B.toString(16);
-
-  return "#" + RR + GG + BB;
 }
